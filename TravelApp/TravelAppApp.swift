6e1e7552b73541592ce06ca6704e9d81a70cfd29
Engin Bolat <engi.bolat@gmail.com>
2024-06-22 23:17:36 +0300

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Engin Bolat on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
